3b4e37e59291efbb307c113b8a405c0e18830dae
Bhavya Manocha <bhavyamanocha849@gmail.com>
2021-08-27 04:36:02 +0530

    changes in following/followers page

--- public/js/followersAndFollowing.js
+++ public/js/followersAndFollowing.js
@@ -1,74 +1,62 @@
-$(document).ready(()=>{
-    getUserLoggedIn()
-    console.log(selectedTab);
-    if(selectedTab === "followers"){
+$(document).ready(() => {
+
+    if(selectedTab === "followers") {
         loadFollowers();
-    }else{
+    }
+    else {
         loadFollowing();
     }
-})
+});
 
-var userLoggedIn;
-
-function getUserLoggedIn(){
-    $.get(`/api/users/current`,(user)=>{
-        userLoggedIn = user;
-        // console.log("uu",userLoggedIn)
-    });
-}
-
-function loadFollowers(){
-    $.get(`/api/users/${profileUserId}/followers`,results=>{
-        outputUsers(results.followers,$(".resultContainer"))
+function loadFollowers() {
+    $.get(`/api/users/${profileUserId}/followers`, results => {
+        outputUsers(results.followers, $(".resultsContainer"));
     })
 }
 
-function loadFollowing(){
-    $.get(`/api/users/${profileUserId}/following`,results=>{
-        outputUsers(results.following,$(".resultContainer"))
+function loadFollowing() {
+    $.get(`/api/users/${profileUserId}/following`, results => {
+        outputUsers(results.following, $(".resultsContainer"));
     })
 }
 
-function outputUsers(results,container){
+function outputUsers(results, container) {
     container.html("");
-    console.log(results)
-    results.forEach(result=>{
-        // console.log("ss",result);
-        var html = createFollowerHtml(result,true)
-        container.append(html);
-    })
 
-    if(results.length == 0){
-        container.append("<span class = 'noResults'>No results found</span>")
+    results.forEach(result => {
+        var html = createUserHtml(result, true);
+        container.append(html);
+    });
+
+    if(results.length == 0) {
+        container.append("<span class='noResults'>No results found</span>")
     }
 }
 
-function createFollowerHtml(userData,showFollowButton){
-    var name = userData.firstName + userData.lastName;
-    var isFollowing = userLoggedIn.following && userLoggedIn.following.includes(userData._id)
+function createUserHtml(userData, showFollowButton) {
 
-    console.log(isFollowing);
-    var text = isFollowing ? "Following":"Follow"
-    console.log(text);
-    var buttonClass = isFollowing ? "followButton following" : "followButton";
+    var name = userData.firstName + " " + userData.lastName;
+    var isFollowing = userLoggedIn.following && userLoggedIn.following.includes(userData._id);
+    var text = isFollowing ? "Following" : "Follow"
+    var buttonClass = isFollowing ? "followButton following" : "followButton"
+
     var followButton = "";
-    if(showFollowButton && userLoggedIn._id !== userData._id)  {
+    if (showFollowButton && userLoggedIn._id != userData._id) {
         followButton = `<div class='followButtonContainer'>
                             <button class='${buttonClass}' data-user='${userData._id}'>${text}</button>
                         </div>`;
     }
 
-
     return `<div class='user'>
-                <div class ='userImageContainer'> 
-                    <img src = '${userData.profilePic}'>
+                <div class='userImageContainer'>
+                    <img src='${userData.profilePic}'>
                 </div>
-                <div class = 'userDetailsContainer'>
+                <div class='userDetailsContainer'>
                     <div class='header'>
-                        <a href = '/profile/${userData.username}'>${name}</a>
+                        <a href='/profile/${userData.username}'>${name}</a>
                         <span class='username'>@${userData.username}</span>
                     </div>
-                </div> 
+                </div>
                 ${followButton}
-            </div>`
+            </div>`;
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,90 +1,60 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
+const bodyParser = require("body-parser")
+const User = require('../../schemas/UserSchema');
+const Post = require('../../schemas/PostSchema');
 
-const bodyParser= require('body-parser');
-const User = require("../../schemas/UserSchema")
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-router.put("/:userId/follow",async(req,res,next)=>{
-    var userID = req.params.userId;
+router.put("/:userId/follow", async (req, res, next) => {
 
-    var user = await User.findById(userID);
-    console.log(user);
-    if(user == null){
-        return res.statusCode(404);
-    }
+    var userId = req.params.userId;
 
-    //check if followers exist
-    //then check if the followers arrays has the user
-     
-    var isFollowing = user.followers && user.followers.includes(req.session.user._id);
-    var option = isFollowing ?"$pull":"$addToSet";
-    req.session.user =  await User.findByIdAndUpdate(req.session.user._id,{[option]:{following:userID}},{new:true})
-    .catch(er=>{
-        console.log(er);
-        res.sendStatus(400);
-    })
-
-    User.findByIdAndUpdate(userID,{[option]:{followers:req.session.user._id}}) 
-    .catch(er=>{
-        console.log(er);
-        res.sendStatus(400);
-    })
+    var user = await User.findById(userId);
     
+    if (user == null) return res.sendStatus(404);
+
+    var isFollowing = user.followers && user.followers.includes(req.session.user._id);
+    var option = isFollowing ? "$pull" : "$addToSet";
+
+    req.session.user = await User.findByIdAndUpdate(req.session.user._id, { [option]: { following: userId } }, { new: true})
+    .catch(error => {
+        console.log(error);
+        res.sendStatus(400);
+    })
+
+    User.findByIdAndUpdate(userId, { [option]: { followers: req.session.user._id } })
+    .catch(error => {
+        console.log(error);
+        res.sendStatus(400);
+    })
+
     res.status(200).send(req.session.user);
 })
 
-router.get("/current",async(req,res,next)=>{
-    var user = await User.findById(req.session.user._id);
-    console.log("user"+user);
-    if(user === null){
-        // console.log();
-        return res.sendStatus(400);
-    }
-    return res.status(200).send(user);
-})
-
-router.get("/:userId/following",async(req,res,next)=>{
-    user = await User.findById(req.params.userId);
-    user.populate("following");
-    if(user == null){
-        console.log(er);
+router.get("/:userId/following", async (req, res, next) => {
+    User.findById(req.params.userId)
+    .populate("following")
+    .then(results => {
+        res.status(200).send(results);
+    })
+    .catch(error => {
+        console.log(error);
         res.sendStatus(400);
-        return;
-    }
-    res.status(200).send(user);
-    // .then((results)=>{
-    //     console.log(results);
-    //     res.status(200).send(results);
-    // })
-    // .catch((er)=>{
-    //     console.log(er);
-    //     res.sendStatus(400);
-    //     return;
-    // })
+    })
 });
 
-
-router.get("/:userId/followers",async(req,res,next)=>{
-    user = await User.findById(req.params.userId);
-    user.populate("following");
-    if(user == null){
-        console.log(er);
+router.get("/:userId/followers", async (req, res, next) => {
+    User.findById(req.params.userId)
+    .populate("followers")
+    .then(results => {
+        res.status(200).send(results);
+    })
+    .catch(error => {
+        console.log(error);
         res.sendStatus(400);
-        return;
-    }
-    res.status(200).send(user);
-    // User.findById(req.params.userId)
-    // .populate("followers")
-    // .then((results)=>{
-    //     res.status(200).send(results);
-    // })
-    // .catch((er)=>{
-    //     console.log(er);
-    //     res.sendStatus(400);
-    // })
+    })
 });
-
 
 module.exports = router;
